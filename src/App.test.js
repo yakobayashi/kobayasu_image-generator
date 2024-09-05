9d883aa91ec1b559ee5ee8059824d5b9a674f1c7
yakobayashi <47299544+yakobayashi@users.noreply.github.com>
2024-09-05 09:09:28 +0000

    CI/CD

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Image Generation with OpenAI/i);
+  expect(titleElement).toBeInTheDocument();
 });
+
